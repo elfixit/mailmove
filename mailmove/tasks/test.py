5e8825d21980ca0bba4d7f6567f3837e885373d6
elfixit <el@xiala.net>
2012-12-09 19:48:29 +0100

    fixing docs and more

--- mailmove/tasks/test.py
+++ mailmove/tasks/test.py
@@ -11,7 +11,15 @@ from mailmove.models import Job, Topic
 
 @celery.task
 def test_topic(job_uuid, topic_uuid):
-    pass
+    """
+        This is the Test a Topic Task
+        -----------------------------
+
+        This method uses the MongoEngine Object Model Factory the find the right model and from there the controller
+    """
+    topic = Topic.objects.get(id=topic_uuid)
+    controller = topic.get_controller()
+    controller.test()
 
 @celery.task
 def test_job(job_uuid):
